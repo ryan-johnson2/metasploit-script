5de84f09fad10f74f54c404d0ce8139db52dc56a
Ryan Johsnon <ryan.v.johnson3@gmail.com>
2017-07-24 21:28:12 -0400

    stuuf
    Signed-off-by: Ryan Johsnon <ryan.v.johnson3@gmail.com>

--- metasploit-script.sh
+++ metasploit-script.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-aptitude install build-essential zlib1g zlib1g-dev libxml2 libxml2-dev libxslt1-dev locate libreadline6-dev libcurl4-openssl-dev git-core libssl-dev libyaml-dev openssl autoconf libtool libncurses5-dev bison curl wget postgresql postgresql-contrib libpq-dev libapr1 libaprutil1 libsvn1 libpcap-dev
+apt-get install build-essential zlib1g zlib1g-dev libxml2 libxml2-dev libxslt1-dev locate libreadline6-dev libcurl4-openssl-dev git-core libssl-dev libyaml-dev openssl autoconf libtool libncurses5-dev bison curl wget postgresql postgresql-contrib libpq-dev libapr1 libaprutil1 libsvn1 libpcap-dev
 apt-get install build-essential libreadline-dev libssl-dev libpq5 libpq-dev libreadline5 libsqlite3-dev libpcap-dev openjdk-7-jre subversion git-core autoconf postgresql pgadmin3 curl zlib1g-dev libxml2-dev libxslt1-dev vncviewer libyaml-dev ruby1.9.3 openjdk-7-jdk nmap -y
 #installs reguired packages
 curl -L https://get.rvm.io | bash -s stable --autolibs=enabled
@@ -13,9 +13,9 @@ gem install nokogiri -- --use-system-libraries
 cd /opt
 wget http://downloads.metasploit.com/data/releases/archive/framework-3.5.0.tar.bz2
 #gets metasploit source from github
-tar -xjfv framework3.5.0.tar.bz2
+tar -xjfv framework-3.5.0.tar.bz2
 #extracts msf
-rm framework3.5.0.tar.bz2
+rm framework-3.5.0.tar.bz2
 #clean up
 cd /opt/msf3
 bash -c 'for MSF in $(ls msf*); do ln -s /opt/metasploit-framework/$MSF /usr/local/bin/$MSF;done'
